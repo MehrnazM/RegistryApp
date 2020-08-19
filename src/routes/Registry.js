c1fb424610b9860397334ebaa8cc6b2af47bf219
Mehrnaz <mehrnaz.movahed@gmail.com>
2020-08-19 23:17:03 +0000

    Hook

--- src/routes/Registry.js
+++ src/routes/Registry.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Registry(){
+    const [registryData, setRegistryData] = useState([])
+    const [textInput, setTextInput] = useState("")
+
+    const addItem = (e) => {
+        e.preventDefault();
+        const tempData = [...registryData];
+        tempData.push(textInput)
+        setRegistryData(tempData)
+        setTextInput("")
+        
+    }
+    console.log(registryData)
     return(
         <dive>
             <h1>Registry</h1>
             <Link to="/">Click here to go to home</Link>
+            <form onSubmit={addItem}>
+                <label>text input: 
+                    <input type="text" value={textInput} onChange ={(e) => setTextInput(e.target.value)}/>
+                </label>
+                <br/><input type="submit" value="Submit" />
+            </form>
         </dive>
     )
 }
