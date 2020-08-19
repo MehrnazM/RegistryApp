b83c593f5b6cad09eb47f95e243b382a5b824cdc
Mehrnaz <mehrnaz.movahed@gmail.com>
2020-08-19 22:43:03 +0000

    Simple Registry

---
+++ src/routes/Registry.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Registry(){
+    return(
+        <dive>
+            <h1>Registry</h1>
+            <Link to="/">Click here to go to home</Link>
+        </dive>
+    )
+}
+
+export default Registry;
